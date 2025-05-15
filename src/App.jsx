891c7dd70f53d08033439181a6685fc57e628843
Luisa Ibarra <ibarratucano@gmail.com>
2025-05-15 10:28:57 -0500

    eliminación archivos innnecesarios

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,9 @@ import Login from './components/login';
 import ProductosCrud from './pages/productos/productosCrud';
 import PedidosCrud from './pages/pedidos/pedidosCrud';
 import Inconveniente from './pages/Inconvenientes/CrudInconvenientes';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
+// import Navbar from './components/navbar';
+// import Footer from './components/footer';
 import ListRestaurants from './pages/Restaurant/List';
-import CreateRestaurant from './pages/Restaurant/create';
-import UpdateRestaurantPage from './pages/Restaurant/update';
 
 
 function AppContent() {
@@ -29,8 +27,6 @@ function AppContent() {
           <Route path="/pedidos" element={<PedidosCrud />} />
           <Route path="/inconvenientes" element={<Inconveniente />} />
           <Route path="/restaurants" element={<ListRestaurants/>} />
-          <Route path="/restaurants/create" element={<CreateRestaurant/>} />
-          {/* <Route path="/restaurants/update/:id" element={<UpdateRestaurantPage/>} /> */}
           {/* Agrega más rutas aquí */}
         </Routes>
       </div>
